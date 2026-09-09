aaf1e2ef21c3e3e331590d3c80e6862385cae284
ana-decodes <solkeanagha@gmail.com>
2026-09-10 02:23:46 +0530

    Add drowsiness alerts monitoring and logging

--- Scripts/alerts.py
+++ Scripts/alerts.py
@@ -1,4 +1,5 @@
 import time
+import winsound
 
 
 class AlertSystem:
@@ -8,14 +9,19 @@ class AlertSystem:
         self.alert_cooldown = 3
 
     def warning(self):
-        print("⚠️ WARNING: Driver is drowsy!")
+        print("WARNING: Driver is drowsy!")
 
     def alarm(self):
         current_time = time.time()
 
         if current_time - self.last_alert_time >= self.alert_cooldown:
 
-            print("🔊 ALARM! WAKE UP!")
+            print("ALARM! WAKE UP!")
+
+            # Play alarm sound
+            winsound.Beep(1000, 500)
+            winsound.Beep(1200, 500)
+            winsound.Beep(1000, 500)
 
             self.last_alert_time = current_time
 
